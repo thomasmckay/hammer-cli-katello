0ea5314e6017bf3f0e76fca64024c17b8b83bc16
Tom McKay <thomasmckay@redhat.com>
2015-08-11 13:54:15 -0400

    attach working

--- lib/hammer_cli_katello/host_collection_subscription.rb
+++ lib/hammer_cli_katello/host_collection_subscription.rb
@@ -3,23 +3,36 @@ module HammerCLIKatello
     command_name "subscription"
     desc _("Manipulate subscriptions for a host collection")
 
-    class AddSubscriptionsCommand < HammerCLIKatello::Command
+    class AddSubscriptionsCommand < HammerCLIKatello::SingleResourceCommand
+      include HammerCLIForemanTasks::Async
       resource :host_collections, :add_subscriptions
+      #action :create
       #action :add_subscriptions
 
       desc "Add subscription"
       command_name "attach"
 
-      #option "--subscription-id", "SUBSCRIPTION_ID", _("ID of subscription"),
-      #       :attribute_name => :option_subscription_id, :required => true
+      option "--subscription-id", "SUBSCRIPTION_ID", _("ID of subscription"),
+             :attribute_name => :option_subscription_id, :required => true
+      option "--quantity", "QUANTITY", _("Subscription quantity (do not specify to indicate automatic)"),
+             :attribute_name => :option_quantity, :required => false
 
-      #build_options do |o|
-      #  o.expand.except(:subscriptions)
-      #  o.without(:subscriptions)
-      #end
+      build_options do |o|
+        o.expand.except(:subscriptions)
+        o.without(:subscriptions)
+      end
 
-      success_message _("Subscription added to activation key")
-      failure_message _("Could not add subscription to activation key")
+      def request_params
+        params = super
+        params[:subscriptions] = [{
+            :id => options['option_subscription_id'],
+            :quantity => options['option_quantity'] || -1
+        }]
+        params
+      end
+
+      success_message _("Subscription added to content hosts in collection")
+      failure_message _("Could not add subscription to content hosts in collection")
     end
 
     class RemoveSubscriptionCommand < HammerCLIKatello::SingleResourceCommand
