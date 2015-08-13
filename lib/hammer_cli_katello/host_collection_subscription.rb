26e6b3bfdbfa9485ac8ad28c58536c1069a8db16
Tom McKay <thomasmckay@redhat.com>
2015-08-13 10:48:45 -0400

    auto-attach working

--- lib/hammer_cli_katello/host_collection_subscription.rb
+++ lib/hammer_cli_katello/host_collection_subscription.rb
@@ -3,13 +3,42 @@ module HammerCLIKatello
     command_name "subscription"
     desc _("Manipulate subscriptions for a host collection")
 
+    class ListSubscriptionsCommand < HammerCLIKatello::ListCommand
+      command_name "list"
+      resource :host_collections, :subscriptions
+
+      output do
+        field :product_name, _("Name")
+        field :contract_number, _("Contract")
+        field :account_number, _("Account")
+        field :support_level, _("Support")
+        field :format_quantity, _("Quantity")
+        field :consumed, _("Consumed")
+        field :end_date, _("End Date")
+        field :id, _("ID")
+        field :product_name, _("Product")
+        field :consumed, _("Attached")
+      end
+
+      def extend_data(data)
+        data["format_quantity"] = data["quantity"] == -1 ? _("Unlimited") : data["quantity"]
+        data
+      end
+
+      build_options
+    end
+
+    class AutoAttachSubscriptionsCommand < HammerCLIKatello::SingleResourceCommand
+      include HammerCLIForemanTasks::Async
+      resource :host_collections, :autoattach_subscriptions
+      command_name "auto-attach"
+
+      build_options
+    end
+
     class AddSubscriptionsCommand < HammerCLIKatello::SingleResourceCommand
       include HammerCLIForemanTasks::Async
       resource :host_collections, :add_subscriptions
-      #action :create
-      #action :add_subscriptions
-
-      desc "Add subscription"
       command_name "attach"
 
       option "--subscription-id", "SUBSCRIPTION_ID", _("ID of subscription"),
@@ -36,7 +65,8 @@ module HammerCLIKatello
     end
 
     class RemoveSubscriptionCommand < HammerCLIKatello::SingleResourceCommand
-      action :remove_subscriptions
+      include HammerCLIForemanTasks::Async
+      resource :host_collections, :remove_subscriptions
 
       desc _("Remove subscription")
       command_name "unattach"
@@ -49,8 +79,17 @@ module HammerCLIKatello
         o.without(:subscriptions)
       end
 
-      success_message _("Subscription removed from activation key")
-      failure_message _("Could not remove subscription from activation key")
+      def request_params
+        params = super
+        params[:subscriptions] = [{
+            :id => options['option_subscription_id'],
+            :quantity => options['option_quantity'] || -1
+        }]
+        params
+      end
+
+      success_message _("Subscription removed from content hosts in collection")
+      failure_message _("Could not remove subscription from content hosts in collection")
     end
 
     autoload_subcommands
